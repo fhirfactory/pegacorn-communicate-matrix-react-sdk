a80a8a888819f6f27b506b784e705d1dc1c66201
samridhis <samridhis@gmail.com>
2021-04-23 16:03:50 +1000

    Updated string name Signin to Login on the login page

--- src/components/views/auth/PasswordLogin.tsx
+++ src/components/views/auth/PasswordLogin.tsx
@@ -484,7 +484,7 @@ export default class PasswordLogin extends React.PureComponent<IProps, IState> {
                     {forgotPasswordJsx}
                     { !this.props.busy && <input className="mx_Login_submit"
                         type="submit"
-                        value={('Log in')}
+                        value={_t('Sign in')}
                         disabled={this.props.disableSubmit}
                     /> }
                 </form>
