07e3920ee9417a1b68e06ce2b54a217a68173adc
Surendra Panday <panday.sp@gmail.com>
2021-06-01 13:27:11 +1000

    formatted directory service file

--- src/DirectoryService.ts
+++ src/DirectoryService.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {MatrixClientPeg} from "./MatrixClientPeg";
+import { MatrixClientPeg } from "./MatrixClientPeg";
 import * as config from './config';
 
 // we have a number of types defined from the Matrix spec which can't reasonably be altered here.
@@ -26,136 +26,136 @@ export const KIND_SERVICE_DIRECTORY_SEARCH = "search_service_directory";
 export const KIND_PEOPLE_DIRECTORY_SEARCH = "search_people_directory";
 
 export const searchIsOnRoleOrPeopleOrServiceDirectory = (kind) => {
-	if (kind === KIND_ROLE_DIRECTORY_SEARCH) {
-		return true;
-	} else if (kind === KIND_PEOPLE_DIRECTORY_SEARCH) {
-		return true;
-	} else if (kind === KIND_SERVICE_DIRECTORY_SEARCH) {
-		return true;
-	} else {
-		return false;
-	}
+    if (kind === KIND_ROLE_DIRECTORY_SEARCH) {
+        return true;
+    } else if (kind === KIND_PEOPLE_DIRECTORY_SEARCH) {
+        return true;
+    } else if (kind === KIND_SERVICE_DIRECTORY_SEARCH) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
  * This will be used by service, role and people search by switching api based on search context
  * RETURN a fetch Promise, so calling code can either
  *        call this method asynchronously:
- *            getFavoritesForCurrentUser(kind).then(response => { 
+ *            getFavoritesForCurrentUser(kind).then(response => {
  *                <code to process response>
- *            }) 
+ *            })
  *        OR synchronously:
  *            const response = await getFavoritesForCurrentUser(kind);
- *            <code to process response> 
- *        The response will be 
+ *            <code to process response>
+ *        The response will be
  *        - an object with an errorText property if an error occurs
  *        - otherwise an object with a favorites property
  */
 export const getFavoritesForCurrentUser = (kind) => {
-	// Find favorites from relevant api (roles / services)
-	// get user id
-	const user_id_encoded = encodeURI(MatrixClientPeg.get().getUserId());
-	let favorite_api = config.communicate_api_base_path + config.prefix + user_id_encoded;
-	if (kind === KIND_ROLE_DIRECTORY_SEARCH) {
-		favorite_api += config.search_by_favorite.role_suffix;
-	}
-	return fetch(favorite_api, {
-		method: "GET",
-		headers: {
-			'Content-Type': 'application/json',
-		}
-	}).then(res => res.json())
-	  .then(response => {
-		return {
-			favorites: response.favourites
-		};
-	}).catch((err) => {
-		return {
-			errorText: err
-		};
-	});
+    // Find favorites from relevant api (roles / services)
+    // get user id
+    const user_id_encoded = encodeURI(MatrixClientPeg.get().getUserId());
+    let favorite_api = config.communicate_api_base_path + config.prefix + user_id_encoded;
+    if (kind === KIND_ROLE_DIRECTORY_SEARCH) {
+        favorite_api += config.search_by_favorite.role_suffix;
+    }
+    return fetch(favorite_api, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }).then(res => res.json())
+        .then(response => {
+            return {
+                favorites: response.favourites
+            };
+        }).catch((err) => {
+            return {
+                errorText: err
+            };
+        });
 }
 
 /**
  * RETURN - null if the specified kind parameter is not supported;
  *        - otherwise a fetch Promise, so calling code can either
  *          call this method asynchronously:
- *              getMatchingRecords(term, kind).then(response => { 
+ *              getMatchingRecords(term, kind).then(response => {
  *                  <code to process response>
- *              }) 
+ *              })
  *          OR synchronously:
  *              const response = await getMatchingRecords(term, kind);
- *              <code to process response> 
- *          The response will be 
+ *              <code to process response>
+ *          The response will be
  *          - an object with an errorText property if an error occurs
- *          - otherwise an object with an numOfRecordsFromSearchAPI and results (an array 
+ *          - otherwise an object with an numOfRecordsFromSearchAPI and results (an array
  *            of objects with display_name, user_id, role_category and roleIsActive properties)
  */
 export const getMatchingRecords = (term: string, kind) => {
-	if (!searchIsOnRoleOrPeopleOrServiceDirectory(kind)) {
-		return null;
-	}
+    if (!searchIsOnRoleOrPeopleOrServiceDirectory(kind)) {
+        return null;
+    }
 
-	let search_api_path;
-	// form an api path based on context
+    let search_api_path;
+    // form an api path based on context
 
-	if (kind === KIND_ROLE_DIRECTORY_SEARCH) {
-		if (!term) {
-			search_api_path = config.search_all_roles
-		} else {
-			search_api_path = config.search_role_by_displayName + term;
-		}
-	}
+    if (kind === KIND_ROLE_DIRECTORY_SEARCH) {
+        if (!term) {
+            search_api_path = config.search_all_roles
+        } else {
+            search_api_path = config.search_role_by_displayName + term;
+        }
+    }
 
-	// Find roles, services, and people
-	return fetch(search_api_path, {
-		method: "GET",
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	}).then(res => {
-		return res.json().then(results => [res, results]);
-	}).then(function([res, results]) {
-		let totalRecords = 0;
+    // Find roles, services, and people
+    return fetch(search_api_path, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => {
+        return res.json().then(results => [res, results]);
+    }).then(function ([res, results]) {
+        let totalRecords = 0;
         if (!results) {
-			results = [];
-		} else {
-		    totalRecords = Number(res.headers.get('X-Total-Count'));
-		}
+            results = [];
+        } else {
+            totalRecords = Number(res.headers.get('X-Total-Count'));
+        }
 
         console.log("totalRecords=" + totalRecords);
-		let mappedResults = results.map(value => ({
-			display_name: value["description"],
-			user_id: value["simplifiedID"],
-			role_category: value["primaryRoleCategoryID"],
-			// filled or not filled status
-			// if activePractitionerSet array is non-empty someone is fulfilling that role
-			roleIsActive: (value.activePractitionerSet?.length >= 1) ?? false
-		}));
+        let mappedResults = results.map(value => ({
+            display_name: value["description"],
+            user_id: value["simplifiedID"],
+            role_category: value["primaryRoleCategoryID"],
+            // filled or not filled status
+            // if activePractitionerSet array is non-empty someone is fulfilling that role
+            roleIsActive: (value.activePractitionerSet?.length >= 1) ?? false
+        }));
         console.log("mappedResults.length=" + mappedResults.length + ", mappedResults=" + JSON.stringify(mappedResults));
 
-		return {
-			numOfRecordsFromSearchAPI: totalRecords,
-			results: mappedResults
-		};
-	}).catch(err => {
-		return {
-			errorText: err
-		};
-	});
+        return {
+            numOfRecordsFromSearchAPI: totalRecords,
+            results: mappedResults
+        };
+    }).catch(err => {
+        return {
+            errorText: err
+        };
+    });
 }
 
 /**
  * This will be used by service, role and people search by switching api based on search context
  * RETURN a fetch Promise, so calling code can either
  *        call this method asynchronously:
- *            getRoleDetail(roleId).then(response => { 
+ *            getRoleDetail(roleId).then(response => {
  *                <code to process response>
- *            }) 
+ *            })
  *        OR synchronously:
  *            const response = await getRoleDetail(roleId);
- *            <code to process response> 
- *        The response will be 
+ *            <code to process response>
+ *        The response will be
  *        - an object with an errorText property if an error occurs
  *        - otherwise an object with roles and activeRoleEmails properties
  */
@@ -164,38 +164,38 @@ export const getRoleDetail = (roleId: string) => {
     const view_role_detail = config.search_all_roles + queryId;
     // api data
     return fetch(view_role_detail, {
-            method: "GET"
-	}).then(res => res.json())
-	  .then(response => {
-		let emails = [];
-		let roleArrayResponse = [];
-		let entries = response.entry;
-		if (entries) {
-			roleArrayResponse.push(entries);
-			roleArrayResponse.map(val => emails = val.activePractitionerSet);
-		}
-		return {
-            roles: roleArrayResponse,
-            activeRoleEmails: emails
-		};
-	}).catch(err => {
-		return {
-			errorText: err
-		};
-	});
+        method: "GET"
+    }).then(res => res.json())
+        .then(response => {
+            let emails = [];
+            let roleArrayResponse = [];
+            let entries = response.entry;
+            if (entries) {
+                roleArrayResponse.push(entries);
+                roleArrayResponse.map(val => emails = val.activePractitionerSet);
+            }
+            return {
+                roles: roleArrayResponse,
+                activeRoleEmails: emails
+            };
+        }).catch(err => {
+            return {
+                errorText: err
+            };
+        });
 }
 
 /**
  * RETURN - null if the id isn't an email
  *        - otherwise a fetch Promise, so calling code can either
  *          call this method asynchronously:
- *              getPractionerDisplayName(id).then(response => { 
+ *              getPractionerDisplayName(id).then(response => {
  *                  <code to process response>
- *              }) 
+ *              })
  *          OR synchronously:
  *              const response = await getPractionerDisplayName(id);
- *              <code to process response> 
- *          The response will be 
+ *              <code to process response>
+ *          The response will be
  *          - an object with an errorText property if an error occurs
  *          - otherwise an object with a displayName property
  */
@@ -205,15 +205,15 @@ export const getPractionerDisplayName = (id: string) => {
     const view_role_detail = config.communicate_api_base_path + config.prefix + queryId;
     // api data
     return fetch(api, {
-            method: "GET"
-	}).then(res => res.json())
-	  .then(response => {
-		return {
-            displayName: response.entry.displayName
-		};
-	}).catch(err => {
-		return {
-			errorText: err
-		};
-	});
+        method: "GET"
+    }).then(res => res.json())
+        .then(response => {
+            return {
+                displayName: response.entry.displayName
+            };
+        }).catch(err => {
+            return {
+                errorText: err
+            };
+        });
 }
